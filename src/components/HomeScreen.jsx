6d8639d3c3b446df05b14ceb2ed1d5ba1245e286
vjastoni <vjastoni13@gmail.com>
2023-01-27 12:22:01 +0800

    adds homeScreen

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomeScreen() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center"></div>
+  );
+}
+
+export default HomeScreen;
