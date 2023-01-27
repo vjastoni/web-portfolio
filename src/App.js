d66b85e1ce1bcc213539bc274dcc5a74be57099f
vjastoni <vjastoni13@gmail.com>
2023-01-27 13:30:13 +0800

    adds the home page content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
 import { IonIcon } from "react-ion-icon";
-import HomeScreen from "./components/HomeScreen";
 
 function App() {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   return (
     <div>
@@ -22,7 +21,7 @@ function App() {
       <div
         className={`${
           open ? "left-[0px] w-[300px]" : "left-[-300px]"
-        } duration-500 h-screen border border-gray-100 absolute`}
+        } duration-500 h-screen border bg-white z-50 border-gray-100 absolute`}
       >
         <ul className="h-screen w-full flex flex-col items-center justify-center gap-2">
           <li className="bg-whitesmoke w-[280px] h-14 p-4 rounded-[100px] flex justify-start items-center gap-1">
@@ -47,9 +46,48 @@ function App() {
           </li>
         </ul>
       </div>
-      {/*Home Screen*/}
-      <div className="w-screen h-screeen bg-[#fcfdfe]">
-        <HomeScreen />
+      {/*Home Page*/}
+      <div className="w-screen h-screeen p-10">
+        <h1 className="font-semibold text-[44px] font-poppins leading-[66px] text-darkGray mt-5">
+          CRAFTING THE BEST
+          <br />
+          ONLINE EXPERIENCE
+        </h1>
+        <p className="text-[15px] font-poppins leading-[30px]">
+          Meet Jastoni Villanueva. A 22-year old web developer in
+          <br /> STI College Global City
+        </p>
+        <div className="flex items-center gap-1 justify-evenly mt-5">
+          <IonIcon
+            size="large"
+            color="dark"
+            name="logo-twitter"
+          ></IonIcon>
+          <IonIcon
+            size="large"
+            color="dark"
+            name="logo-facebook"
+          ></IonIcon>
+          <IonIcon
+            size="large"
+            color="dark"
+            name="logo-instagram"
+          ></IonIcon>
+          <IonIcon
+            size="large"
+            color="dark"
+            name="logo-linkedin"
+          ></IonIcon>
+          <IonIcon
+            size="large"
+            color="dark"
+            name="logo-google"
+          ></IonIcon>
+        </div>
+        <img
+          className="mt-5"
+          src={require("./assets/header-image.jpg")}
+        />
       </div>
     </div>
   );
--- src/components/HomeScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function HomeScreen() {
-  return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center"></div>
-  );
-}
-
-export default HomeScreen;
